8cb8e43225638238258d51c4aae39b17e99af02e
ardighe7@gmail.com <ardighe7@gmail.com>
2023-06-22 20:06:30 +0530

    new file:   min_cost_climbing_stairs.cpp

---
+++ min_cost_climbing_stairs.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int> &cost,int n,vector<int> &dp)
+{
+    if(n==0)
+    {
+        return cost[0];
+    }
+    if(n==1)
+    {
+        return cost[1];
+    }
+    if(dp[n]!=-1)
+    {
+        return dp[n];
+    }
+
+    dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> cost(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>cost[i];
+    }
+
+    vector<int> dp(n+1);
+    for(int i=0;i<n;i++)
+    {
+        dp[i]=-1;
+    }
+
+    cout<<min(solve(cost,n-1,dp),solve(cost,n-2,dp))<<endl;
+
+    return 0;
+}
